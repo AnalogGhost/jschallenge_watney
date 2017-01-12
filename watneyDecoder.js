2e446dbee3f39fba324693703dbf9f22bc763a6b
Christopher Grabski <grabc59@gmail.com>
2017-01-12 13:21:27 -0700

    encode converts a string into an array of hex values

--- watneyDecoder.js
+++ watneyDecoder.js
@@ -1,13 +1,21 @@
+'use strict';
 function decode(str){
 
 }
 
 function encode(str){
- 
+  let hex = [];
+  for (var i = 0; i < str.length; i++) {
+    // let character = str[i];
+    hex.push(str[i].charCodeAt(0).toString(16));
+  }
+ // charCodeAt(0).toString(16)
+ console.log(hex);
 }
 
 function encodeArc(str) {
 
 }
+encode('apple');
 
 module.exports = {decode,encode,encodeArc}
